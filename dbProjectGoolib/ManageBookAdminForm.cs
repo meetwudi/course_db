5f82e46a9a998df1298df37531b4d603983fc47b
Minghao Xu <xianyuhuanyue@gmail.com>
2014-07-26 17:23:53 +0800

    updata 推荐书籍界面 这个是最终版

---
+++ dbProjectGoolib/ManageBookAdminForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace dbProjectGoolib
+{
+    public partial class ManageBookAdminForm : dbProjectGoolib.AdminBasic
+    {
+        public ManageBookAdminForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- dbProjectGoolib/RecommendationAdminForm.Designer.cs
+++ dbProjectGoolib/RecommendationAdminForm.Designer.cs
@@ -34,11 +34,14 @@
             this.Author = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DeleteItem_btn = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.RecommendationList_dataGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // RecommendationList_dataGridView
             // 
+            this.RecommendationList_dataGridView.AllowUserToAddRows = false;
             this.RecommendationList_dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.RecommendationList_dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.ISBN,
@@ -46,6 +49,7 @@
             this.Author});
             this.RecommendationList_dataGridView.Location = new System.Drawing.Point(44, 123);
             this.RecommendationList_dataGridView.Name = "RecommendationList_dataGridView";
+            this.RecommendationList_dataGridView.ReadOnly = true;
             this.RecommendationList_dataGridView.Size = new System.Drawing.Size(355, 150);
             this.RecommendationList_dataGridView.TabIndex = 1;
             // 
@@ -53,16 +57,19 @@
             // 
             this.ISBN.HeaderText = "ISBN";
             this.ISBN.Name = "ISBN";
+            this.ISBN.ReadOnly = true;
             // 
             // BookName
             // 
             this.BookName.HeaderText = "书名";
             this.BookName.Name = "BookName";
+            this.BookName.ReadOnly = true;
             // 
             // Author
             // 
             this.Author.HeaderText = "作者";
             this.Author.Name = "Author";
+            this.Author.ReadOnly = true;
             // 
             // label1
             // 
@@ -83,10 +90,30 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(241, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "下面两个控件全部是实验用的 没用 可以删掉";
+            // 
+            // DeleteItem_btn
+            // 
+            this.DeleteItem_btn.Location = new System.Drawing.Point(297, 291);
+            this.DeleteItem_btn.Name = "DeleteItem_btn";
+            this.DeleteItem_btn.Size = new System.Drawing.Size(75, 23);
+            this.DeleteItem_btn.TabIndex = 5;
+            this.DeleteItem_btn.Text = "删除该项";
+            this.DeleteItem_btn.UseVisualStyleBackColor = true;
+            // 
             // RecommendationAdminForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.ClientSize = new System.Drawing.Size(459, 362);
+            this.Controls.Add(this.DeleteItem_btn);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.RecommendationList_dataGridView);
@@ -94,6 +121,8 @@
             this.Controls.SetChildIndex(this.RecommendationList_dataGridView, 0);
             this.Controls.SetChildIndex(this.label1, 0);
             this.Controls.SetChildIndex(this.button1, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.DeleteItem_btn, 0);
             ((System.ComponentModel.ISupportInitialize)(this.RecommendationList_dataGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -108,5 +137,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Author;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button DeleteItem_btn;
     }
 }
